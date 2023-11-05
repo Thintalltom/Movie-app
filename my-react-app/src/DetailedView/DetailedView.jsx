1a5232c79733965c7361375347461b0cfda84316
tomide <tomideadeyanju@gmail.com>
2023-11-05 20:00:32 +0100

    added new component

--- my-react-app/src/DetailedView/DetailedView.jsx
+++ my-react-app/src/DetailedView/DetailedView.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./DetailedView.css";
 
 const DetailedView = () => {
   const { itemId } = useParams(); // Get the ID from the route parameter
@@ -29,48 +30,65 @@ const DetailedView = () => {
     getFilms();
   }, [itemId]);
 
-  const { writers, title, year, description, trailer_embed_link,
-    big_image, director, genre, trailer, rating } = films;
+  const {
+    writers,
+    title,
+    year,
+    description,
+    trailer_embed_link,
+    big_image,
+    director,
+    genre,
+    trailer,
+    rating,
+  } = films;
 
   return (
-    <div className="flex justify-center gap-[20px] p-4 mt-[10px] items-center bg-slate-900 text-white">
-      <div >
+    <>
+      <div className=" bigScreen flex justify-center  gap-[20px] p-4  items-center bg-slate-900 text-white">
         <div>
-          <p className="text-3xl font-extrabold">{films.title}</p>
-          {films.trailer && (
-            <iframe
-              width="1000"
-              height="500"
-              src={films.trailer_embed_link
-              } // Provide the YouTube video URL here
-              title="YouTube Video"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-          )}
-          <div className="flex flex-col gap-[20px]">
-          <div className="font-light"> Movie Year: {films.year}</div>
-      <div className="font-light "> Movie genre:  
-      {Array.isArray(films.genre) ? (
-             <p>{films.genre.join(',')}</p>
-            
-          ) : (
-            <p>{films.genre}</p>
-          )}
-         </div>
-          <div className=" w-[700px]  font-light  "> Movie description: {films.description}</div>
-          <div className="font-light"> Movies rating: {films.rating}</div>
-          <div className="font-light gap-4">Movies Writer:      {Array.isArray(films.writers) ? (
-             <p>{films.writers.join(',')}</p>
-            
-          ) : (
-            <p>{films.writers}</p>
-          )}</div>
+          <div className=' p-4'>
+            <p className="lg:text-3xl sm:text-xl font-extrabold">{films.title}</p>
+            {films.trailer && (
+              <iframe
+                width="1000"
+                height="500"
+                src={films.trailer_embed_link} // Provide the YouTube video URL here
+                title="YouTube Video"
+                frameBorder="0"
+                allowFullScreen
+                className="film bg-pink-300"
+              ></iframe>
+            )}
+            <div className="flex flex-col gap-[20px] smallerScreen">
+              <div className="font-light"> Movie Year: {films.year}</div>
+              <div className="font-light ">
+                {" "}
+               
+                {Array.isArray(films.genre) ? (
+                  <p> Movie genre: {films.genre.join(",")}</p>
+                ) : (
+                  <p>  Movie genre: {films.genre}</p>
+                )}
+              </div>
+              <div className=" lg:w-[700px] sm:w-[200px]  font-light description ">
+                {" "}
+                Movie description: {films.description} <br />
+              </div>
+              <div className="font-light"> Movies rating: {films.rating}</div>
+              <div className="font-light gap-4">
+                Movies Writer:{" "}
+                {Array.isArray(films.writers) ? (
+                  <p>{films.writers.join(",")}</p>
+                ) : (
+                  <p>{films.writers}</p>
+                )}
+              </div>
+            </div>
           </div>
-
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
